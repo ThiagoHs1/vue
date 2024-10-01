dbf335be71f2b72cc0215f0c4b4b2174394e653e
Thiago Henrique Heinle Sanchez <thiagohenque@live.com>
2024-10-01 15:37:23 -0300

    adicionando botão

--- vue-jobs/src/App.vue
+++ vue-jobs/src/App.vue
@@ -10,7 +10,7 @@ import JobListings from '@/components/JobListings.vue'
 <Navbar />
 <Hero  />
 <HomeCards />
-<JobListings limit="3" />
+<JobListings limit="3" :showButton="true" />
 
 </template>
 
