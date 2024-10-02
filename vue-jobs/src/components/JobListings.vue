801b06fc5187cc33c27c56e59caf1b43a6c77230
Thiago Henrique Heinle Sanchez <thiagohenque@live.com>
2024-10-02 11:06:24 -0300

    display data dinamicamente nos jobs

--- vue-jobs/src/components/JobListings.vue
+++ vue-jobs/src/components/JobListings.vue
@@ -1,11 +1,28 @@
 <script setup>
 import { RouterLink } from 'vue-router';
 import JobListin from './JobListin.vue';
-import JobData from '@/jobs.json';
-import { ref, defineProps } from 'vue';
+import axios from 'axios';
+import { reactive, defineProps, onMounted } from 'vue';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
 
 
-const jobs = ref(JobData);
+const state = reactive({
+  jobs: [],
+  isLoading: true,
+});
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://localhost:5000/jobs');
+    state.jobs = response.data;
+  } catch (error) {
+    console.error('Error fetching jobs', error);
+  } finally {
+    state.isLoading = false;
+  }
+});
+
+
 
 defineProps({
   limit: Number,
@@ -22,9 +39,12 @@ defineProps({
       <h2 class="text-3xl font-bold text-green-500 text-center mb-6">
         Serviços
       </h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+      
+      <div v-if="state.isLoading" class=" text-center text-gray-500 py-6 "  > <PulseLoader /></div>
+
+      <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
         <JobListin
-          v-for="job in jobs.slice(0, limit || jobs.length)"
+          v-for="job in state.jobs.slice(0, limit || state.jobs.length)"
           :key="job.id"
           :job="job"
         />
